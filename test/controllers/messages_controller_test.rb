3d4bcfead9a5f262ffee5aaa2c720d265e804568
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2024-10-08 01:09:57 -0700

    Don't edit messages

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -9,35 +9,4 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
     get messages_url
     assert_response :success
   end
-
-  test "should get new" do
-    get new_message_url
-    assert_response :success
-  end
-
-  test "should create message" do
-    assert_difference("Message.count") do
-      post messages_url, params: { message: { body: @message.body, from: @message.from, subject: @message.subject } }
-    end
-
-    assert_redirected_to message_url(Message.last)
-  end
-
-  test "should get edit" do
-    get edit_message_url(@message)
-    assert_response :success
-  end
-
-  test "should update message" do
-    patch message_url(@message), params: { message: { body: @message.body, from: @message.from, subject: @message.subject } }
-    assert_redirected_to message_url(@message)
-  end
-
-  test "should destroy message" do
-    assert_difference("Message.count", -1) do
-      delete message_url(@message)
-    end
-
-    assert_redirected_to messages_url
-  end
 end
