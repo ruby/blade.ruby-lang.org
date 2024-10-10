d79a51f30cd1696bec29482d26e0d7f472371697
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2024-10-09 21:13:50 -0700

    Add a search form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,25 @@
 class MessagesController < ApplicationController
+  PER_PAGE = 50
+
   # GET /messages or /messages.json
   def index
     query = params[:q]
-    if query
-      # %> and <-> are defined by pg_trgm.
-      # https://www.postgresql.org/docs/17/pgtrgm.html
-      @messages = Message.find_by_sql([
-        'SELECT * FROM messages WHERE body %> ? ORDER BY body <-> ? LIMIT 20',
-        query, query,
-      ])
-    else
-      @messages = Message.all
+    unless query
+      @messages = []
+      return
     end
+
+    page = params[:page].to_i
+    list_ids = get_list_ids(params)
+
+    # %> and <-> are defined by pg_trgm.
+    # https://www.postgresql.org/docs/17/pgtrgm.html
+    message_where = if Rails.env.production?
+      Message.where('body %> ? AND list_id IN (?) ORDER BY body <-> ?', query, list_ids, query)
+    else
+      Message.where('body LIKE ? AND list_id IN (?)', "%#{query}%", list_ids)
+    end
+    @messages = message_where.offset(page * PER_PAGE).limit(PER_PAGE)
   end
 
   # GET /messages/1 or /messages/1.json
@@ -22,5 +30,16 @@ class MessagesController < ApplicationController
       list = List.find_by_name(params[:list_name])
       @message = Message.find_by(list_id: list.id, list_seq: params[:list_seq])
     end
+  end
+
+  private
+  def get_list_ids(params)
+    list_ids = []
+    ['ruby-talk', 'ruby-core', 'ruby-list', 'ruby-dev'].each do |name|
+      if params[name.tr('-', '_').to_sym] == '1'
+        list_ids << List.find_by_name(name).id
+      end
+    end
+    list_ids
   end
 end
