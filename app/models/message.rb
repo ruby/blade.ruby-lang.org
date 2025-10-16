4e62345aa1317614673a1b2b341ea6e13e0b94cb
Akira Matsuda <ronnie@dio.jp>
2025-10-16 20:59:01 +0900

    Mail body may sometimes be broken, but let's proceed anyway
    e.g. ruby-dev 1553, 2320, 2321, 2322, 4361, etc.

--- app/models/message.rb
+++ app/models/message.rb
@@ -12,7 +12,12 @@ class Message < ApplicationRecord
 
   class << self
     def from_mail(mail, list, list_seq)
-      body = mail.body.decoded.encode Encoding::UTF_8, Encoding::ISO_2022_JP
+      body = begin
+        mail.body.decoded.encode Encoding::UTF_8, Encoding::ISO_2022_JP
+      rescue Encoding::InvalidByteSequenceError
+        mail.body.decoded.encode Encoding::UTF_8, Encoding::ISO_2022_JP, invalid: :replace, undef: :replace
+      end
+
       from = mail.from_address.decoded
       new list_id: list.id, list_seq: list_seq, body: body, subject: mail.subject, from: from, published_at: mail.date, message_id_header: mail.message_id
     end
