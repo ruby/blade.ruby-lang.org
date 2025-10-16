f1943f50cef03578766e0d2eba490ad1bf9f1d76
Akira Matsuda <ronnie@dio.jp>
2025-10-16 12:27:49 +0900

    <<

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,54 +4,54 @@ BLADE_BUCKET_NAME = 'blade.ruby-lang.org'
 require 'kconv'
 
 class Message < ApplicationRecord
-    # Not really sure we will utlize this configuration,
-    # but I don't want to make this column.
-    # https://blade.ruby-lang.org/ruby-talk/1 is JST.
-    # https://blade.ruby-lang.org/ruby-talk/410000 is not.
+  # Not really sure we will utlize this configuration,
+  # but I don't want to make this column.
+  # https://blade.ruby-lang.org/ruby-talk/1 is JST.
+  # https://blade.ruby-lang.org/ruby-talk/410000 is not.
   self.skip_time_zone_conversion_for_attributes = [:published_at]
 
   def self.from_s3(list_name, list_seq, s3_client = Aws::S3::Client.new(region: BLADE_BUCKET_REGION))
     obj = s3_client.get_object(bucket: BLADE_BUCKET_NAME, key: "#{list_name}/#{list_seq}")
 
-      m = self.from_string(obj.body.read)
-      m.list_id = List.find_by_name(list_name).id
-      m.list_seq = list_seq
-      m
+    m = self.from_string(obj.body.read)
+    m.list_id = List.find_by_name(list_name).id
+    m.list_seq = list_seq
+    m
   end
 
   def self.from_string(str)
-      # There are a few hacks to import messages from blade.ruby-lang.org's
-      # S3 bucket.
+    # There are a few hacks to import messages from blade.ruby-lang.org's
+    # S3 bucket.
 
-      # Need to call String#b. There are messages that have headers in non-UTF8,
-      # but the body is in UTF-8, such as ruby-list:2882.
+    # Need to call String#b. There are messages that have headers in non-UTF8,
+    # but the body is in UTF-8, such as ruby-list:2882.
     headers_str, body = str.b.split(/\n\n/, 2)
 
-      # ruby-list:2840 doesn't have a proper From header.
-      headers_str = Kconv.toutf8(headers_str).gsub(/\r\n/, '')
+    # ruby-list:2840 doesn't have a proper From header.
+    headers_str = Kconv.toutf8(headers_str).gsub(/\r\n/, '')
 
-      headers = headers_str.split(/\n/).map { |line|
-        line.split(/:\s+/, 2)
-      }.to_h
+    headers = headers_str.split(/\n/).map { |line|
+      line.split(/:\s+/, 2)
+    }.to_h
 
-      published_at = DateTime.strptime(headers['Date'], '%Y-%m-%dT%H:%M:%S%:z')
+    published_at = DateTime.strptime(headers['Date'], '%Y-%m-%dT%H:%M:%S%:z')
 
-      self.new(
-          body: Kconv.toutf8(body),
-          subject: headers['Subject'],
-          from: headers['From'],
-          published_at: published_at,
-      )
+    self.new(
+      body: Kconv.toutf8(body),
+      subject: headers['Subject'],
+      from: headers['From'],
+      published_at: published_at,
+    )
   end
 
   def reload_from_s3(s3_client = Aws::S3::Client.new(region: BLADE_BUCKET_REGION))
     m = Message.from_s3(List.find_by_id(self.list_id).name, self.list_seq, s3_client)
 
-      self.body = m.body
-      self.subject = m.subject
-      self.from = from
-      self.published_at = m.published_at
+    self.body = m.body
+    self.subject = m.subject
+    self.from = from
+    self.published_at = m.published_at
 
-      m
+    m
   end
 end
--- import.rb
+++ import.rb
@@ -3,8 +3,8 @@ require 'optparse'
 params = {}
 OptionParser.new do |opts|
   opts.on('--list LIST')
-    opts.on('--from FROM', Integer)
-    opts.on('--to TO', Integer)
+  opts.on('--from FROM', Integer)
+  opts.on('--to TO', Integer)
 end.parse!(into: params)
 
 list = params[:list]
