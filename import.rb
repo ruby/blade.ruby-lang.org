5bb275bee420e5cddde2c939f6a6b262041355a1
Akira Matsuda <ronnie@dio.jp>
2025-10-16 12:28:14 +0900

    Use transaction

--- import.rb
+++ import.rb
@@ -9,15 +9,17 @@ end.parse!(into: params)
 
 list = params[:list]
 
-(params[:from]..params[:to]).each do |seq|
-  begin
-    message = Message.from_s3(list, seq)
-    message.save
-rescue ActiveRecord::RecordNotUnique
-  STDERR.puts("#{list}:#{seq} already exists in Postgres")
-rescue Aws::S3::Errors::NoSuchKey
-  STDERR.puts("#{list}:#{seq} doesn't exist in S3")
-rescue StandardError => e
-  STDERR.puts("failed to import #{list}:#{seq}: #{e}")
+Message.transaction do
+  (params[:from]..params[:to]).each do |seq|
+    begin
+      message = Message.from_s3(list, seq)
+      message.save!
+    rescue ActiveRecord::RecordNotUnique
+      STDERR.puts("#{list}:#{seq} already exists in Postgres")
+    rescue Aws::S3::Errors::NoSuchKey
+      STDERR.puts("#{list}:#{seq} doesn't exist in S3")
+    rescue StandardError => e
+      STDERR.puts("failed to import #{list}:#{seq}: #{e}")
+    end
   end
 end
