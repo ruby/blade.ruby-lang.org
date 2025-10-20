44b3e69e5ba80c6fe9ce39742e621878869ecdd1
Akira Matsuda <ronnie@dio.jp>
2025-10-20 19:05:00 +0900

    rubocop --only Layout/CommentIndentation,Layout/FirstArrayElementIndentation,Layout/IndentationConsistency,Layout/ArrayAlignment -a

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -7,18 +7,18 @@ module MessagesHelper
   def search_snippet(body, keyword)
     snippet = ''
 
-      offset = 0
-      while (i = body.index(keyword, offset))
-        start = [i - MARGIN, offset].max
-          len = keyword.length + MARGIN
-          snippet += body[start, len]
-          offset = start + len
-      end
+    offset = 0
+    while (i = body.index(keyword, offset))
+      start = [i - MARGIN, offset].max
+      len = keyword.length + MARGIN
+      snippet += body[start, len]
+      offset = start + len
+    end
 
-      if snippet.empty?
-        return body[0, MARGIN * 2]
-      else
-        snippet
-      end
+    if snippet.empty?
+      return body[0, MARGIN * 2]
+    else
+      snippet
+    end
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,16 +1,16 @@
 class List
   def initialize(name, id)
     @name = name
-      @id = id
+    @id = id
   end
   attr_reader :name, :id
 
-    # Ordered by the established dates. ruby-list was started in 1995.
+  # Ordered by the established dates. ruby-list was started in 1995.
   LISTS = [
-      List.new('ruby-list', 1),
-      List.new('ruby-dev', 2),
-      List.new('ruby-core', 3),
-      List.new('ruby-talk', 4),
+    List.new('ruby-list', 1),
+    List.new('ruby-dev', 2),
+    List.new('ruby-core', 3),
+    List.new('ruby-talk', 4),
   ]
 
   def self.find_by_name(name)
