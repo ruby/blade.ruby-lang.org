5f481f71652f16d7503cca45e991ac834a22108f
Akira Matsuda <ronnie@dio.jp>
2025-10-22 01:26:21 +0900

    Handle application/ms-tnef; as an attachment
    ruby-dev: 1148

--- app/models/message.rb
+++ app/models/message.rb
@@ -72,6 +72,9 @@ class Message < ApplicationRecord
       attachments.attach(io: file, filename: part.filename, content_type: part.content_type)
     else
       case part.content_type.downcase
+      when 'application/ms-tnef'
+        file = StringIO.new(part.decoded)
+        attachments.attach(io: file, filename: part.filename || 'noname', content_type: part.content_type)
       when /^text\/plain/, /text\/enriched;/
         (self.body ||= '') << Kconv.toutf8(part.body.raw_source)
       when /^text\/html;/
