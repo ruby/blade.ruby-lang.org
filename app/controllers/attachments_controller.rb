edb8ccbf017c735b9ef31e587eb1973650a549c4
Akira Matsuda <ronnie@dio.jp>
2025-10-24 04:37:57 +0900

    Download blob from DB and send_data from attachments#show

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,4 +1,22 @@
+# frozen_string_literal: true
+
 class AttachmentsController < ApplicationController
+  skip_forgery_protection
+
   def show
+    if (key = decode_verified_key)
+      send_data ActiveStorage::Blob.service.download(key[:key]), content_type: key[:content_type], disposition: key[:disposition]
+    else
+      head :not_found
+    end
+  rescue Errno::ENOENT
+    head :not_found
+  end
+
+  private
+
+  def decode_verified_key
+    key = ActiveStorage.verifier.verified(params[:encoded_key], purpose: :blob_key)
+    key&.deep_symbolize_keys
   end
 end
