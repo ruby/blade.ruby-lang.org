d32f366f20bfb4f1ba892b5554b1fc33174715f9
Akira Matsuda <ronnie@dio.jp>
2025-10-24 12:01:47 +0900

    gomi

--- test/controllers/attachments_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class AttachmentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get attachments_show_url
-    assert_response :success
-  end
-end
